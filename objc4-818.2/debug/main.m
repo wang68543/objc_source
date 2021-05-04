4973261802328e29878e9f288071cdbd5c6207d8
wang68543 <wang68543@163.com>
2021-05-05 01:18:23 +0800

    源码调试成功

---
+++ objc4-818.2/debug/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  debug
+//
+//  Created by 王强 on 2021/5/5.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        NSObject *obj = [[NSObject alloc] init];
+    }
+    return 0;
+}
