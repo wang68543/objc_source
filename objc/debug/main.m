ebf11aa290ee069340eefe56e2a182c1db4a87e0
wang68543 <wang68543@163.com>
2022-06-04 13:11:31 +0800

    添加 MacOS 12 源码调试

---
+++ objc/debug/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  debug
+//
+//  Created by 王强 on 2022/6/4.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSObject *obj = [[NSObject alloc] init];
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
